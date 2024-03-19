437fda9bc84694b7135a633ebf6fd0e005e2147e
Ao Zhong <hacc1225@gmail.com>
2024-03-19 22:00:09 +0100

    Allow activation of analytics cookies without refreshing the page

--- achelper.php
+++ achelper.php
@@ -29,21 +29,49 @@ class ACHelper extends Module
         $this->registerHook('displayHeader') &&
         $this->registerHook('displayBeforeBodyClosingTag') &&
         Configuration::updateValue('ACHelper_GMeasureID', '') &&
-        Configuration::updateValue('ACHelper_TopLevelDomain', '');
+        Configuration::updateValue('ACHelper_TopLevelDomain', '') &&
+        Configuration::updateValue('ACHelper_lgcookieslawID', '');
     }
 
     public function uninstall()
     {
         Configuration::deleteByName('ACHelper_GMeasureID');
         Configuration::deleteByName('ACHelper_TopLevelDomain');
+        Configuration::deleteByName('ACHelper_lgcookieslawID');
         return parent::uninstall();
     }
 
     public function hookDisplayHeader($params)
     {
         return "<script type=\"text/javascript\">
+                    //Setup Consent for Matomo
                     var _paq = window._paq = window._paq || [];
                     _paq.push(['requireCookieConsent']);
+                    _paq.push(['trackPageView']);
+                    
+                    //Setup Consent for Google Analytics
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('consent', 'default', {
+                        'ad_storage': 'denied',
+                        'ad_user_data': 'denied',
+                        'ad_personalization': 'denied',
+                        'analytics_storage': 'denied'
+                    });
+
+                    //Activate tracking code
+                    function allConsentGranted() {
+                        //Activate Matomo with cookies
+                        _paq.push(['rememberCookieConsentGiven']);
+
+                        //Activate Google Analytics
+                        gtag('consent', 'update', {
+                            'ad_user_data': 'granted',
+                            'ad_personalization': 'granted',
+                            'ad_storage': 'granted',
+                            'analytics_storage': 'granted'
+                        });
+                    }
                 </script>";
     }
 
@@ -78,6 +106,12 @@ class ACHelper extends Module
                         'label' => $this->l('Google Measurement ID'),
                         'class' => 't',
                         'name' => 'ACHelper_GMeasureID'
+                    ),
+                    array(
+                        'type' => 'text',
+                        'label' => $this->l('lgcookieslaw plugin purpose ID'),
+                        'class' => 't',
+                        'name' => 'ACHelper_lgcookieslawID'
                     )
                 ),
                 'submit' => array(
@@ -101,6 +135,7 @@ class ACHelper extends Module
         $helper->token = Tools::getAdminTokenLite('AdminModules');
         $helper->fields_value['ACHelper_TopLevelDomain'] = Configuration::get('ACHelper_TopLevelDomain');
         $helper->fields_value['ACHelper_GMeasureID'] = Configuration::get('ACHelper_GMeasureID');
+        $helper->fields_value['ACHelper_lgcookieslawID'] = Configuration::get('ACHelper_lgcookieslawID');
         return $helper->generateForm(array($fields_form));
     }
 
@@ -108,13 +143,12 @@ class ACHelper extends Module
     {
         Configuration::updateValue('ACHelper_TopLevelDomain', Tools::getValue('ACHelper_TopLevelDomain'));
         Configuration::updateValue('ACHelper_GMeasureID', Tools::getValue('ACHelper_GMeasureID'));
+        Configuration::updateValue('ACHelper_lgcookieslawID', Tools::getValue('ACHelper_lgcookieslawID'));
     }
     
     public function hookDisplayBeforeBodyClosingTag($params)
     {
         return "<script type=\"text/javascript\">
-                    _paq.push(['trackPageView']);
-
                     function ACHelperDeleteCookie(name, domain, path) {
                         let cookieString = name + \"=; expires=Thu, 01 Jan 1970 00:00:00 GMT\";
                         if (domain) {
@@ -126,15 +160,43 @@ class ACHelper extends Module
                         document.cookie = cookieString;
                     }
 
-                    if (typeof lgcookieslaw_cookie_values === 'object' &&
-                    lgcookieslaw_cookie_values.lgcookieslaw_purpose_3 === true
-                    ) {
-                        _paq.push(['rememberCookieConsentGiven']);
-                    }
-                    else{
-                        _paq.push(['forgetCookieConsentGiven']);
-                        ACHelperDeleteCookie('_ga_".Configuration::get('ACHelper_GMeasureID')."', '.".Configuration::get('ACHelper_TopLevelDomain')."', '/');
-                        ACHelperDeleteCookie('_ga', '.".Configuration::get('ACHelper_TopLevelDomain')."', '/');
+                    if (typeof lgcookieslaw_cookie_values === 'object') {
+                        if (lgcookieslaw_cookie_values === null) {
+                            //Confirm Cookies consent status when the user first enters the page (lgcookieslaw_cookie_values is null at this time)
+
+                            // Select the element that controls the consent for Analytics Cookies
+                            const cookiesConsentToggle = document.getElementById('lgcookieslaw_slider_".Configuration::get('ACHelper_lgcookieslawID')."');
+
+                            // Define a function to handle changes in the consent toggle for cookies
+                            const onConsentToggleChange = function(mutationsList, observer) {
+                                for (let mutation of mutationsList) {
+                                    if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                                        const currentClass = cookiesConsentToggle.className;
+                                        // Check if the class attribute matches our target state
+                                        if (currentClass.includes('lgcookieslaw-slider') && currentClass.includes('lgcookieslaw-slider-checked')) {
+                                            allConsentGranted();
+                                        }
+                                        consentObserver.disconnect();
+                                    }
+                                }
+                            }
+
+                            // Instantiate MutationObserver with a callback to monitor attribute changes
+                            const consentObserver = new MutationObserver(onConsentToggleChange);
+
+                            // Start observing the class attribute changes of the cookies consent toggle
+                            consentObserver.observe(cookiesConsentToggle, { attributes: true });
+                        }
+                        else {
+                            if (lgcookieslaw_cookie_values.lgcookieslaw_purpose_".Configuration::get('ACHelper_lgcookieslawID')." === true) {
+                                allConsentGranted();
+                            }
+                            else {
+                                _paq.push(['forgetCookieConsentGiven']);
+                                ACHelperDeleteCookie('_ga_".Configuration::get('ACHelper_GMeasureID')."', '.".Configuration::get('ACHelper_TopLevelDomain')."', '/');
+                                ACHelperDeleteCookie('_ga', '.".Configuration::get('ACHelper_TopLevelDomain')."', '/');
+                            }
+                        }
                     }
                 </script>";
     }
