468a6f3de37a5db21fa81e6ebbd04f992876f66a
Ao Zhong <hacc1225@gmail.com>
2024-03-20 01:37:49 +0100

    Move the script to be inserted into Smarty instead of PHP

--- achelper.php
+++ achelper.php
@@ -43,35 +43,7 @@ class ACHelper extends Module
 
     public function hookDisplayHeader($params)
     {
-        return "<script type=\"text/javascript\">
-                    //Setup Consent for Matomo
-                    var _paq = window._paq = window._paq || [];
-                    _paq.push(['requireCookieConsent']);
-                    
-                    //Setup Consent for Google Analytics
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('consent', 'default', {
-                        'ad_storage': 'denied',
-                        'ad_user_data': 'denied',
-                        'ad_personalization': 'denied',
-                        'analytics_storage': 'denied'
-                    });
-
-                    //Activate tracking code
-                    function allConsentGranted() {
-                        //Activate Matomo with cookies
-                        _paq.push(['rememberCookieConsentGiven']);
-
-                        //Activate Google Analytics
-                        gtag('consent', 'update', {
-                            'ad_user_data': 'granted',
-                            'ad_personalization': 'granted',
-                            'ad_storage': 'granted',
-                            'analytics_storage': 'granted'
-                        });
-                    }
-                </script>";
+        return $this->context->smarty->fetch($this->local_path.'views/templates/hook/header.tpl');
     }
 
     public function getContent()
@@ -147,59 +119,12 @@ class ACHelper extends Module
     
     public function hookDisplayBeforeBodyClosingTag($params)
     {
-        return "<script type=\"text/javascript\">
-                    _paq.push(['trackPageView']);
-
-                    function ACHelperDeleteCookie(name, domain, path) {
-                        let cookieString = name + \"=; expires=Thu, 01 Jan 1970 00:00:00 GMT\";
-                        if (domain) {
-                            cookieString += \"; domain=\" + domain;
-                        }
-                        if (path) {
-                            cookieString += \"; path=\" + path;
-                        }
-                        document.cookie = cookieString;
-                    }
-
-                    if (typeof lgcookieslaw_cookie_values === 'object') {
-                        if (lgcookieslaw_cookie_values === null) {
-                            //Confirm Cookies consent status when the user first enters the page (lgcookieslaw_cookie_values is null at this time)
-
-                            // Select the element that controls the consent for Analytics Cookies
-                            const cookiesConsentToggle = document.getElementById('lgcookieslaw_slider_".Configuration::get('ACHelper_lgcookieslawID')."');
-
-                            // Define a function to handle changes in the consent toggle for cookies
-                            const onConsentToggleChange = function(mutationsList, observer) {
-                                for (let mutation of mutationsList) {
-                                    if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-                                        const currentClass = cookiesConsentToggle.className;
-                                        // Check if the class attribute matches our target state
-                                        if (currentClass.includes('lgcookieslaw-slider') && currentClass.includes('lgcookieslaw-slider-checked')) {
-                                            allConsentGranted();
-                                        }
-                                        consentObserver.disconnect();
-                                    }
-                                }
-                            }
-
-                            // Instantiate MutationObserver with a callback to monitor attribute changes
-                            const consentObserver = new MutationObserver(onConsentToggleChange);
-
-                            // Start observing the class attribute changes of the cookies consent toggle
-                            consentObserver.observe(cookiesConsentToggle, { attributes: true });
-                        }
-                        else {
-                            if (lgcookieslaw_cookie_values.lgcookieslaw_purpose_".Configuration::get('ACHelper_lgcookieslawID')." === true) {
-                                allConsentGranted();
-                            }
-                            else {
-                                _paq.push(['forgetCookieConsentGiven']);
-                                ACHelperDeleteCookie('_ga_".Configuration::get('ACHelper_GMeasureID')."', '.".Configuration::get('ACHelper_TopLevelDomain')."', '/');
-                                ACHelperDeleteCookie('_ga', '.".Configuration::get('ACHelper_TopLevelDomain')."', '/');
-                            }
-                        }
-                    }
-                </script>";
+        $this->context->smarty->assign(array(
+            'TopLevelDomain' => Configuration::get('ACHelper_TopLevelDomain'),
+            'GMeasureID' => Configuration::get('ACHelper_GMeasureID'),
+            'lgcookieslawID' => Configuration::get('ACHelper_lgcookieslawID'),
+        ));
+        return $this->context->smarty->fetch($this->local_path.'views/templates/hook/before-body-closing-tag.tpl');
     }
 
 }
